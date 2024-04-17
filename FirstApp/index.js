e2f386171d30bbdbadb31855a48c1fe271ca6567
sdevesh890 <74634378+sdevesh890@users.noreply.github.com>
2024-04-17 10:04:25 +0530

    Creating Dynamic HTML With Templating

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -1,30 +1,43 @@
 const express = require('express');
 const  app = express();
-
-
+const Path = require('path');
+const redditData = require('./data.json');
 // app.use((req , res)=>
 // {
 //     // console.log("WE GOT THE NEW REQUEST!!!");
 //     res.send('<h1>HELLLOO!!! THIS IS OUR RESPONSE</h1>')
 // })
 
+app.use(express.static(Path.join(__dirname , 'assets')));
+
+// app.set('view engine','ejs');
+app.set('views' , Path.join(__dirname , '/views')); //Recommended
 app.get('/',(req,res)=>{
-    res.send('THIS IS MY HOMEPAGE');
+    // res.send('THIS IS MY HOMEPAGE');
+    const name = 'Devesh Sharma';
+    res.render('home.ejs' , {boy : name});
 })
 app.get('/cats',(req,res)=>
 {
     res.send('MEOW!!!');
 })
 
-app.get('/dogs',(req,res)=>
+app.get('/Math',(req,res)=>
 {
-    res.send('WOOF!!');
+    const num = Math.floor(Math.random()*10)+1;
+    res.render('Math.ejs' , {num});
 })
 
-app.get('/r/:subpath',(req,res)=>
+app.get('/r/:subreddit',(req,res)=>
 {
-    const {subpath} = req.params;
-    res.send(`THIS IS THE HOMEPAGE OF ${subpath}`);
+    const {subreddit} = req.params;
+    const data = redditData[subreddit];
+    if (data) {
+        res.render('subreddit.ejs',{...data});
+    }else 
+    {
+        res.render('notFound.ejs',{subreddit});
+    }
 })
 
 app.get('*',(req,res)=>
